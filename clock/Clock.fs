e7f47d4d0aec6e85678235a45905187db527b8d1
Marcin Golenia <marcingolenia@gmail.com>
2018-12-29 22:31:14 +0100

    clock problem solved, robot problem solved.

--- clock/Clock.fs
+++ clock/Clock.fs
@@ -3,16 +3,36 @@ module Clock
 [<Measure>] type min
 [<Measure>] type h
 
+[<Literal>] 
+let minutesInDay = 1440<min>
+
+[<Literal>] 
+let minutesPerHour = 60<min/h>  
+
+let toMinutes hours =
+    hours * minutesPerHour
+
+let toHours minutes =
+    minutes / minutesPerHour
+
 type Clock = { Hours: int<h>; Minutes:int<min> }
 
+let getTotalMinutes clock =
+    (clock.Hours |> toMinutes) + clock.Minutes 
+
+let add minutes clock = 
+    let minutesToAdd = minutes * 1<min> % minutesInDay
+    let timeToSetInMinutes = (minutesInDay + getTotalMinutes clock + minutesToAdd) % minutesInDay
+    {Hours = timeToSetInMinutes |> toHours; Minutes = timeToSetInMinutes % 60<min>}
+
 let create hours minutes =
-    let trueHours = (minutes / 60 + hours) % 24 * 1<h>
-    let trueMinutes = minutes % 60 * 1<min>
-    { Hours = trueHours; Minutes = trueMinutes }
+    let totalMinutes = minutes * 1<min> + (hours * 1<h> |> toMinutes)
+    add (totalMinutes/1<min>) {Hours = 0<h>; Minutes = 0<min>}
 
 let display clock =
     sprintf "%02i:%02i" clock.Hours clock.Minutes
 
-let add minutes clock = failwith "You need to implement this function."
-
-let subtract minutes clock = failwith "You need to implement this function."
+let subtract minutesToSubstruct clock =
+    let minutesToSubstruct = minutesToSubstruct * 1<min> % minutesInDay
+    let timeToSetInMinutes = (minutesInDay + getTotalMinutes clock - minutesToSubstruct) % minutesInDay
+    {Hours = timeToSetInMinutes |> toHours; Minutes = timeToSetInMinutes % 60<min>}
---
+++ robot-simulator/RobotSimulator.fs
@@ -0,0 +1,35 @@
+﻿module RobotSimulator
+
+type Direction = North | East | South | West
+type Position = int * int
+type Robot = { direction: Direction; position: Position }
+
+let create direction position =
+    {direction = direction; position = position}
+
+let turnRobot command robot =
+    match (command, robot.direction) with
+    | ('R', Direction.North) | ('L', Direction.South) -> { robot with direction = Direction.East }
+    | ('R', Direction.East) | ('L', Direction.West) -> { robot with direction = Direction.South }
+    | ('R', Direction.South) | ('L', Direction.North) -> { robot with direction = Direction.West }
+    | ('R', Direction.West) | ('L', Direction.East) -> { robot with direction = Direction.North }
+    | _ -> failwith "Invalid direction"
+
+let advance robot =
+    match robot.direction with
+    | Direction.North -> { robot with position = (robot.position |> fst, (robot.position |> snd) + 1)}
+    | Direction.East -> { robot with position = ((robot.position |> fst) + 1, robot.position |> snd)}
+    | Direction.South -> { robot with position = (robot.position |> fst, (robot.position |> snd) - 1)}
+    | Direction.West -> { robot with position = ((robot.position |> fst) - 1, robot.position |> snd)}
+    | _ -> failwith "Invalid direction"
+
+let executeCommand robot command =
+    match command with
+    | 'R' | 'L' -> robot |> turnRobot command
+    | 'A' -> robot |> advance
+    | _ -> failwith "Incorrect command"
+
+let move instructions robot =
+    instructions 
+                |> List.ofSeq 
+                |> List.fold (fun robotState command -> executeCommand robotState command) robot
