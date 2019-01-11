49d9582a47a66a06beef2ee134a9fdd4507bf6cb
Marcin Golenia <marcingolenia@gmail.com>
2019-01-11 23:44:24 +0100

    allergies problem - commit tests

---
+++ allergies/Allergies.fs
@@ -0,0 +1,9 @@
+﻿module Allergies
+
+open System
+
+// TODO: define the Allergen type
+
+let allergicTo codedAllergies allergen = failwith "implement this function."
+
+let list codedAllergies = failwith "implement this function."
