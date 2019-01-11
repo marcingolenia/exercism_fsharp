5213cbeb2fff5452d5aaec502d7eb2a38a2a2ee4
Marcin Golenia <marcingolenia@gmail.com>
2019-01-11 23:27:58 +0100

    improvments after code-review

--- robot-simulator/RobotSimulator.fs
+++ robot-simulator/RobotSimulator.fs
@@ -7,7 +7,7 @@ type Robot = { direction: Direction; position: Position }
 let create direction position =
     {direction = direction; position = position}
 
-let turnRobot command robot =
+let private turnRobot command robot =
     match (command, robot.direction) with
     | ('R', Direction.North) | ('L', Direction.South) -> { robot with direction = Direction.East }
     | ('R', Direction.East) | ('L', Direction.West) -> { robot with direction = Direction.South }
@@ -15,21 +15,19 @@ let turnRobot command robot =
     | ('R', Direction.West) | ('L', Direction.East) -> { robot with direction = Direction.North }
     | _ -> failwith "Invalid direction"
 
-let advance robot =
-    match robot.direction with
-    | Direction.North -> { robot with position = (robot.position |> fst, (robot.position |> snd) + 1)}
-    | Direction.East -> { robot with position = ((robot.position |> fst) + 1, robot.position |> snd)}
-    | Direction.South -> { robot with position = (robot.position |> fst, (robot.position |> snd) - 1)}
-    | Direction.West -> { robot with position = ((robot.position |> fst) - 1, robot.position |> snd)}
-    | _ -> failwith "Invalid direction"
+let private advance ({direction = direction; position = (x, y)} as robot) =
+    match direction with
+    | Direction.North -> { robot with position = (x, y + 1)}
+    | Direction.East  -> { robot with position = (x + 1, y)}
+    | Direction.South -> { robot with position = (x, y - 1)}
+    | Direction.West  -> { robot with position = (x - 1, y)}
 
-let executeCommand robot command =
+let private executeCommand robot command =
     match command with
     | 'R' | 'L' -> robot |> turnRobot command
     | 'A' -> robot |> advance
     | _ -> failwith "Incorrect command"
 
 let move instructions robot =
-    instructions 
-                |> List.ofSeq 
-                |> List.fold (fun robotState command -> executeCommand robotState command) robot
+    (robot, instructions) 
+        ||> Seq.fold (fun robotState command -> executeCommand robotState command)
