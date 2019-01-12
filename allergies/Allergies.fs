353e73376934a4d73b3a6e38b3a74a46e722ee40
Marcin Golenia <marcingolenia@gmail.com>
2019-01-12 15:08:44 +0100

    Added editorconfig, sovled Allergens problem.

--- allergies/Allergies.fs
+++ allergies/Allergies.fs
@@ -1,9 +1,23 @@
-﻿module Allergies
+module Allergies
 
 open System
 
-// TODO: define the Allergen type
+[<FlagsAttribute>]
+type Allergen =
+  | Eggs = 1
+  | Peanuts = 2
+  | Shellfish = 4
+  | Strawberries = 8
+  | Tomatoes = 16
+  | Chocolate = 32
+  | Pollen = 64
+  | Cats = 128
 
-let allergicTo codedAllergies allergen = failwith "implement this function."
+let allergicTo codedAllergies (allergen: Allergen) =
+  int (enum<Allergen>(codedAllergies) &&& allergen) <> 0
 
-let list codedAllergies = failwith "implement this function."
+let list codedAllergies =
+  let decodedAllergies = enum<Allergen>(codedAllergies)
+  Enum.GetValues(typeof<Allergen>) :?> Allergen[]
+    |> Array.filter (fun allergen -> decodedAllergies.HasFlag(allergen))
+    |> Array.toList
